8a73e65c2b321bddbf644338a318a0098acca165
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-07 19:38:00 -0600

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import { Header } from './components/header'
 
 function App() {
   return (
     <div>
-      My Dope Page
+      <Header />
     </div>
   );
 }
