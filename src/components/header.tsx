d9a35cc247a25a030c12949c9ed7693b047ab0e8
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-14 08:44:15 -0600

    Add home, hello, and h1 to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export const Header: React.FC = () => {
   return (
-    <header className="header-base bg-main-300">
-      <p>Dope Header</p>
+    <header className="header-base">
+      <div className="flex flex-row justify-between">
+        <button>Home Btn with icon</button>
+        <button>Say Hello.</button>
+      </div>
+      <br/>
+      <div>
+        <h1>hi, my name is Garrett.</h1>
+        <h2>a front-end developer</h2>
+      </div>
     </header>
   )
 }
