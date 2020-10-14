dec67d056965fe6462104e96e3df7ece234a3f9a
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-14 14:06:18 -0600

    Add header images, add method to display them on the DOM

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import images from '../assets/images/images'
+import React, { ReactElement } from 'react';
+import { headerImages }from '../assets/images/images'
 
 export const Header: React.FC = () => {
+
+  const getHeaderImages = ():JSX.Element[] => {
+    return headerImages.headerIcons.map(val => {
+      return <img src={`${val}`} alt="Decorative icon" className="h-16 w-16 m-6"/>
+      })
+  }
+
   return (
     <header className="header-base">
       <div className="flex flex-row justify-between">
-        <img role="button" src={images.gIcon} alt="G icon" className="h-12 w-12 m-6"/>
+        <img role="button" src={headerImages.gIcon} alt="G icon" className="h-12 w-12 m-6"/>
         <button className="base-button w-32 m-6 p-2 text-main-purple-dark">Say Hello.</button>
       </div>
       <br/>
       <div className="flex flex-col items-center mt-20">
         <h1 className="text-6xl font-bold">hi, my name is Garrett</h1>
         <h2 className="text-xl font-medium">front-end developer.</h2>
+      </div>
+      <div className="flex flex-row"> 
+        {getHeaderImages()}
       </div>
     </header>
   )
