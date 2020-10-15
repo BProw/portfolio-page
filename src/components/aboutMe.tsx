c40f7c628959531109b5b466f85cafce5a597a10
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-15 15:58:52 -0600

    Add layout for skills sections

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -4,8 +4,8 @@ export const AboutMe: React.FC = () => {
   return (
     <section className="base-container-height w-full flex bg-main-purple-light">
       <div className="w-1/3 m-auto">
-        <h2 className="mb-4 text-center text-3xl font-bold">Nice to meet you.</h2>
-        <p className="leading-loose text-xl">
+        <h2 className="mb-4 text-center text-3xl font-bold">nice to meet you.</h2>
+        <p className="pb-32 leading-loose text-xl">
           I'm baby migas banjo cold-pressed affogato jean shorts af, pickled poke cred seitan hammock. 
           VHS keytar semiotics poutine pour-over taiyaki fam cornhole small batch scenester hoodie banjo 
           kombucha hell of. Blue bottle drinking vinegar cardigan bespoke banjo iPhone. Pickled lo-fi fixie twee cray. 
--- src/components/skillsLayout.tsx
+++ src/components/skillsLayout.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 export const SkillsLayout: React.FC = () => {
   return(
     <section className="base-container-height w-full m-auto">
-      <div className="w-full h-full m-auto relative">
-        <div className="flex flex-col">
-          <p className="m-auto h-64 w-5/6 text-center bg-white font-bold text-black text-2xl rounded-t-lg">Here</p>
-          {/* <p className="m-auto h-48 w-5/6 text-center bg-white font-bold text-2xl">Here</p>
-          <p className="m-auto h-48 w-5/6 text-center bg-white font-bold text-2xl rounded-b-lg">Here</p> */}
+      <div className="w-full h-full m-auto -mt-40 relative">
+        <div className="w-2/3 m-auto flex flex-col">
+          <div className="flex flex-row">
+          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-black text-xl rounded-tl-lg">languages</h3>
+          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-xl">front-end developer</h3>
+          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-xl rounded-tr-lg">background</h3>
+          </div>
         </div>
       </div>
     </section>
