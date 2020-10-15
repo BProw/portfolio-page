9c5104ab47e7cf99bc3846f0a22397bb4c2628dd
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-15 09:34:12 -0600

    Add basic layout for the about me section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/header'
+import { AboutMe } from './components/aboutMe'
 
 function App() {
   return (
     <div>
       <Header />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/aboutMe.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const AboutMe: React.FC = () => {
+
+
+
+
+  return (
+    <section className="h-64 w-full mt-10">
+      <div className="w-6/12 m-auto">
+        <h2 className="mb-2 text-center text-xl font-bold">Nice to meet you.</h2>
+        <p className="leading-loose">
+          I'm baby migas banjo cold-pressed affogato jean shorts af, pickled poke cred seitan hammock. 
+          VHS keytar semiotics poutine pour-over taiyaki fam cornhole small batch scenester hoodie banjo 
+          kombucha hell of. Blue bottle drinking vinegar cardigan bespoke banjo iPhone. Pickled lo-fi fixie twee cray. 
+          Mixtape poutine schlitz chicharrones 8-bit glossier helvetica. Yuccie poutine crucifix coloring 
+          book twee godard biodiesel adaptogen gochujang af street art.
+        </p>
+      </div>
+    </section>
+  )
+}
