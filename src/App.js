34330efb5d11d1c5d4b6a8faa2d41cd7835ccb1c
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-15 14:37:42 -0600

    Refactor container heights

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './components/header'
 import { AboutMe } from './components/aboutMe'
+import { SkillsLayout } from './components/skillsLayout'
 
 function App() {
   return (
     <div>
       <Header />
       <AboutMe />
+      <SkillsLayout />
     </div>
   );
 }
---
+++ src/components/skillsLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SkillsLayout: React.FC = () => {
+  return(
+    <section className="base-container-height w-full m-auto bg-white">
+      <div className="w-full h-full m-auto bg-main-gray-blue-medium opacity-75">
+        xcsdc
+      </div>
+    </section>
+  )
+}
