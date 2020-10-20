bf78f9e102368df924b2697c027a4d50803159a6
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-20 09:47:34 -0600

    Add test data

--- src/assets/data/projectData.tsx
+++ src/assets/data/projectData.tsx
@@ -1,26 +1,26 @@
 export const projectData: { title: string, description: string, imagePath: string, link: string }[] = [
   {
-    title: '',
-    description: '',
-    imagePath: '',
-    link: ''
+    title: 'Test 1',
+    description: 'Test 1',
+    imagePath: 'Test 1',
+    link: 'Test 1'
   },
   {
-    title: '',
-    description: '',
-    imagePath: '',
-    link: ''
+    title: 'Test 2',
+    description: 'Test 2',
+    imagePath: 'Test 2',
+    link: 'Test 2'
   },
   {
-    title: '',
-    description: '',
-    imagePath: '',
-    link: ''
+    title: 'Test 3',
+    description: 'Test 3',
+    imagePath: 'Test 3',
+    link: 'Test 3'
   },
   {
-    title: '',
-    description: '',
-    imagePath: '',
-    link: ''
+    title: 'Test 4',
+    description: 'Test 4',
+    imagePath: 'Test 4',
+    link: 'Test 4'
   }
 ]
