e1d75a9ec19b3c71f08d03004e118e14cc7aa74e
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-20 09:47:17 -0600

    Display data for projects on page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { projectData } from '../assets/data/projectData'
 
 export const Projects: React.FC = () => {
+
+  const displayProjectData = ():JSX.Element[] => {
+    return projectData.map(project => {
+      return(
+        <div>
+          <h1>{project.title}</h1>
+          <img src={project.imagePath} alt={`Display image for ${project.title}`} />
+          <p>{project.description}</p>
+          <a href={project.link}>Check Me</a>
+        </div>
+      )
+    })
+  }
+
   return (
     <section className="base-container-height w-full flex bg-main-purple-light">
-        Projects
+      {displayProjectData()}
     </section>
   )
 }
