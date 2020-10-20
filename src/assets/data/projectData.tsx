cdd7293d431405852f6e981dc3f7219718a7a8fa
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-20 09:33:38 -0600

    Add data folder and file to hold project data

---
+++ src/assets/data/projectData.tsx
@@ -0,0 +1,26 @@
+export const projectData: { title: string, description: string, imagePath: string, link: string }[] = [
+  {
+    title: '',
+    description: '',
+    imagePath: '',
+    link: ''
+  },
+  {
+    title: '',
+    description: '',
+    imagePath: '',
+    link: ''
+  },
+  {
+    title: '',
+    description: '',
+    imagePath: '',
+    link: ''
+  },
+  {
+    title: '',
+    description: '',
+    imagePath: '',
+    link: ''
+  }
+]
