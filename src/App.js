ab0531533df0168f902e5909619a72b6644d7538
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-26 15:16:15 -0600

    Add base layout for testimonials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './components/header'
 import { AboutMe } from './components/aboutMe'
 import { SkillsLayout } from './components/skillsLayout'
 import { Projects } from './components/projects'
+import { Testimonials } from './components/testimonials'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AboutMe />
       <SkillsLayout />
       <Projects />
+      <Testimonials />
     </div>
   );
 }
---
+++ src/components/testimonials.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Testimonials: React.FC = () => {
+  return (
+    <div className="base-container-height">
+      Testimonials
+    </div>
+    )
+}
