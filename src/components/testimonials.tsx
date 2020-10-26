76727e908fc430081467ab19a12f1ff046fd78f5
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-26 15:44:49 -0600

    Add first testimonial

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -4,22 +4,16 @@ export const Testimonials: React.FC = () => {
   return (
     <div className="base-container-height w-1/3 m-auto flex flex-col justify-center">
       <h2 className="text-2xl font-bold text-center">Testimonials</h2>
-      <h3 className="text-xs text-center">People say nice things!</h3>
-      <img src="" alt="Picture of "/>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris 
-        pellentesque pulvinar pellentesque. Non blandit massa enim nec dui nunc mattis enim. Dignissim 
-        suspendisse in est ante in nibh. Adipiscing at in tellus integer feugiat. Nunc sed velit 
-        dignissim sodales ut eu. Auctor augue mauris augue neque gravida in fermentum et sollicitudin. 
-        Turpis egestas integer eget aliquet nibh praesent. Quisque non tellus orci ac auctor augue 
-        mauris augue. Arcu non sodales neque sodales ut etiam. Eget est lorem ipsum dolor sit 
-        amet consectetur. Euismod nisi porta lorem mollis. Interdum velit laoreet id donec ultrices 
-        tincidunt arcu non sodales. Semper eget duis at tellus at. Faucibus turpis in eu mi bibendum 
-        neque egestas congue. Leo in vitae turpis massa sed.
+      <h3 className="text-xs mb-4 text-center">People say nice things!</h3>
+      <p className="mb-1">
+        I had the pleasure to work with Garrett during his contract at Frontier Airlines. 
+        Garrett is a talented developer with a keen mind for problem solving and collaboration. 
+        During his time at Frontier, he exceeded expectations, quickly becoming a valuable 
+        asset to the team. It is without reservation that I can recommend Garrett for future
+        positions as a software engineer.
       </p>
-      <h3 className="text-xs text-center">Their name</h3>
-      <h3 className="text-xs text-center">Their role</h3>
+      <h3 className="text-xs text-center">- Brandon Haller -</h3>
+      <h3 className="text-xs text-center">Sr. Software Engineer at Frontier Airlines</h3>
     </div>
     )
 }
