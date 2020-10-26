0339b045ece5ace40ff3bf14db12eba2eb0e5aa8
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-26 15:27:59 -0600

    Add template info for testimonials

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export const Testimonials: React.FC = () => {
   return (
-    <div className="base-container-height">
-      Testimonials
+    <div className="base-container-height w-1/3 m-auto flex flex-col justify-center">
+      <h2 className="text-2xl font-bold text-center">Testimonials</h2>
+      <h3 className="text-xs text-center">People say nice things!</h3>
+      <img src="" alt="Picture of "/>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris 
+        pellentesque pulvinar pellentesque. Non blandit massa enim nec dui nunc mattis enim. Dignissim 
+        suspendisse in est ante in nibh. Adipiscing at in tellus integer feugiat. Nunc sed velit 
+        dignissim sodales ut eu. Auctor augue mauris augue neque gravida in fermentum et sollicitudin. 
+        Turpis egestas integer eget aliquet nibh praesent. Quisque non tellus orci ac auctor augue 
+        mauris augue. Arcu non sodales neque sodales ut etiam. Eget est lorem ipsum dolor sit 
+        amet consectetur. Euismod nisi porta lorem mollis. Interdum velit laoreet id donec ultrices 
+        tincidunt arcu non sodales. Semper eget duis at tellus at. Faucibus turpis in eu mi bibendum 
+        neque egestas congue. Leo in vitae turpis massa sed.
+      </p>
+      <h3 className="text-xs text-center">Their name</h3>
+      <h3 className="text-xs text-center">Their role</h3>
     </div>
     )
 }
