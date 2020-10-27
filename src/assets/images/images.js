561d547f5f07d95ecfa9c0328ee253550f5e6213
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-27 15:17:27 -0600

    Add social media icons

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -4,10 +4,16 @@ import deskTop from "./personal-computer.png"
 import phone from "./smart-phone.png"
 import laptop from "./laptop.png"
 import deskPlant from "./plant.png"
+import mail from "./mail.png"
+import twitter from "./twitter.png"
+import github from "./github.png"
+import linkedin from "./linkedin.png"
 
 export const headerImages = {
   gIcon,
   headerIcons: [frenchPress, deskTop, phone, laptop, deskPlant],
 }
 
-// export default images
+export const footerImages = {
+  socailMediaIcons: [mail, twitter, github, linkedin],
+}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const Footer: React.FC = () => {
   return(
-    <footer className="base-container-height bg-main-purple-light">
-      Footer
+    <footer className="base-container-height bg-main-purple-light border-t-2 border-main-purple-dark">
+      <div className="flex flex-row justify-center">
+        <div className="text-center pt-3 rounded-full border border-main-purple-dark w-12 h-12">email</div>
+        <div className="text-center pt-2 rounded-full border border-main-purple-dark w-12 h-12">T</div>
+        <div className="text-center pt-2 rounded-full border border-main-purple-dark w-12 h-12">GH</div>
+        <div className="text-center pt-2 rounded-full border border-main-purple-dark w-12 h-12">LI</div>
+      </div>
     </footer>
   )
 }
