aa141d3c72c71929ac3b6c6666ce62a41154223c
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-27 17:14:30 -0600

    Add style for starting a project box in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,14 +16,19 @@ export const Footer: React.FC = () => {
   return (
     <footer className="base-container-height bg-main-purple-light border-t-2 border-main-purple-dark">
       <div className="h-64 w-2/3 m-auto -mt-32 border rounded bg-main-gray-blue-medium">
-        <div className="flex flex-row justify-evenly mt-24">
-          <h3 className="text-white text-3xl font-bold">Let's start something!</h3>
-          <h4 className="text-white text-lg font-bold">Interested in working together?<br />Email me.</h4>
-          <div role="button" className="flex flex-col justify-center w-56 h-16 text-center text-white border-2 rounded-full">Let's go!</div>
+        <div className="flex flex-row justify-evenly mt-12">
+          <p className="text-white text-3xl font-bold">
+            <strong>Interested in working together?</strong> 
+            <br />
+            Let's start something
+            <br />
+            Email me.
+          </p>
+          <div role="button" className="flex flex-col justify-center w-56 h-16 mt-10 text-center text-white border-2 rounded-full">Let's go!</div>
         </div>
       </div>
-      <a href=""><img src={footerImages.gIcon} alt="G icon back to home" className="h-10 w-10 m-auto mt-24"/></a>
-      <h3 className="text-center">Some phrase or words to sum it up</h3>
+      <a href=""><img src={footerImages.gIcon} alt="G icon back to home" className="h-10 w-10 m-auto mt-32" /></a>
+      <h3 className="text-center my-8">Some phrase or words to sum it up</h3>
       <div className="flex flex-row justify-center">
         {displaySocialMediaIcons()}
       </div>
