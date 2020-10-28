c3c1f54ce8a05bc3852a181a01b01c6d26fd4726
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-10-28 08:37:00 -0600

    Add hover style for footer button

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,14 +17,14 @@ export const Footer: React.FC = () => {
     <footer className="base-container-height bg-main-purple-light border-t-2 border-main-purple-dark">
       <div className="h-64 w-2/3 m-auto -mt-32 border rounded bg-main-gray-blue-medium">
         <div className="flex flex-row justify-evenly mt-12">
-          <p className="text-white text-3xl font-bold">
+          <p className="text-white text-3xl font-semibold">
             <strong>Interested in working together?</strong> 
             <br />
             Let's start something
             <br />
             Email me.
           </p>
-          <div role="button" className="flex flex-col justify-center w-56 h-16 mt-10 text-center text-white border-2 rounded-full">Let's go!</div>
+          <div role="button" className="footer-button flex flex-col justify-center w-56 h-16 mt-10 font-semibold text-center text-white border-2 rounded-full">Let's go!</div>
         </div>
       </div>
       <a href=""><img src={footerImages.gIcon} alt="G icon back to home" className="h-10 w-10 m-auto mt-32" /></a>
