d71d9a7e7d251a7963c49b0206c70499e953800f
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-11-04 18:48:21 -0700

    Add bullets for skills

--- src/components/skillsLayout.tsx
+++ src/components/skillsLayout.tsx
@@ -6,17 +6,22 @@ export const SkillsLayout: React.FC = () => {
       <div className="w-full h-full m-auto -mt-40 relative">
         <div className="w-2/3 m-auto flex flex-col">
           <div className="flex flex-row">
-          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-black text-xl rounded-tl-lg">languages</h3>
+          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-black text-xl rounded-tl-lg">languages | frameworks</h3>
           <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-xl">front-end developer</h3>
           <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-xl rounded-tr-lg">background</h3>
           </div>
         </div>
         <div className="flex flex-row justify-center justify-evenly w-full">
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+            <li>JavaScript</li>
+            <li>React | Redux</li>
+            <li>Vue | Nuxt</li>
+            <li>TypeScript</li>
+            <li>HTML</li>
+            <li>CSS | SCSS | Tailwaind</li>
+            <li>Node</li>
+            <li>React Native</li>
+            <li>jQuery</li>
           </ul>
           <ul>
             <li>1</li>
@@ -25,10 +30,10 @@ export const SkillsLayout: React.FC = () => {
             <li>4</li>
           </ul>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+            <li>Sales</li>
+            <li>Team Leadership</li>
+            <li>Mentorship</li>
+            <li>Overall Positivity</li>
           </ul>
         </div>
       </div>
