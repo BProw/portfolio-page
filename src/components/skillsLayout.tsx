abdd4c611818f392081d0d2bb739771d7d015e66
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-11-04 19:00:17 -0700

    Combine columns into one

--- src/components/skillsLayout.tsx
+++ src/components/skillsLayout.tsx
@@ -3,38 +3,42 @@ import React from 'react'
 export const SkillsLayout: React.FC = () => {
   return(
     <section className="base-container-height w-full m-auto">
-      <div className="w-full h-full m-auto -mt-40 relative">
-        <div className="w-2/3 m-auto flex flex-col">
+      <div className="w-2/3 h-full m-auto border">
           <div className="flex flex-row">
-          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-black text-xl rounded-tl-lg">languages | frameworks</h3>
-          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-xl">front-end developer</h3>
-          <h3 className="m-auto pt-20 h-48 w-5/6 text-center bg-white font-bold text-xl rounded-tr-lg">background</h3>
           </div>
-        </div>
         <div className="flex flex-row justify-center justify-evenly w-full">
-          <ul>
-            <li>JavaScript</li>
-            <li>React | Redux</li>
-            <li>Vue | Nuxt</li>
-            <li>TypeScript</li>
-            <li>HTML</li>
-            <li>CSS | SCSS | Tailwaind</li>
-            <li>Node</li>
-            <li>React Native</li>
-            <li>jQuery</li>
-          </ul>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-          </ul>
-          <ul>
-            <li>Sales</li>
-            <li>Team Leadership</li>
-            <li>Mentorship</li>
-            <li>Overall Positivity</li>
-          </ul>
+          <div className="flex flex-col">
+            <h3 className="text-center bg-white font-bold text-black text-xl">languages | frameworks</h3>
+            <ul>
+              <li>JavaScript</li>
+              <li>React | Redux</li>
+              <li>Vue | Nuxt</li>
+              <li>TypeScript</li>
+              <li>HTML</li>
+              <li>CSS | SCSS | Tailwaind</li>
+              <li>Node</li>
+              <li>React Native</li>
+              <li>jQuery</li>
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-center bg-white font-bold text-xl">front-end developer</h3>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-center bg-white font-bold text-xl">background</h3>
+            <ul>
+              <li>Sales</li>
+              <li>Team Leadership</li>
+              <li>Mentorship</li>
+              <li>Overall Positivity</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
