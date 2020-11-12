6ff9b9af9b719fc5da47d147818ddc6c6be69097
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-11-12 13:06:49 -0700

    Add role tags

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -42,11 +42,11 @@ export const Projects: React.FC = () => {
         >
           <div className="flex flex-col m-auto pl-5">
             <h2 className="text-white text-2xl font-semibold text-center m-2">Tweet311Denver</h2>
-            <img src={projectIcons.denver} alt="" className="h-32 w-32 m-auto mt-5" />
+            <img src={projectIcons.denver} alt="Denver art museum icon" className="h-32 w-32 m-auto mt-5" />
           </div>
           <div className={projectOneHover ? "flex flex-col m-auto items-center duration-1000 ease-in-out" : "flex flex-col m-auto items-center opacity-0 duration-700 ease-in-out transform scale-75"}>
             <p className="w-2/3 m-5 text-center text-white text-base">An application to improve the saftey of the commuters of Denver.</p>
-            <a href={projectData[0].link} target="_blank" className="w-2/3 p-2 text-white text-center border rounded-full hover:bg-white hover:text-black">Watch Demo</a>
+            <a href={projectData[0].link} target="_blank" role="button" className="w-2/3 p-2 text-white text-center border rounded-full hover:bg-white hover:text-black">Watch Demo</a>
           </div>
         </div>
 
@@ -56,12 +56,12 @@ export const Projects: React.FC = () => {
           onMouseLeave={toggleProjectTwoHover}
         >
           <div className="flex flex-col w-1/2 m-auto pl-5">
-            <img src={projectIcons.brush} alt="" className="h-32 w-32 m-auto mt-5" />
+            <img src={projectIcons.brush} alt="Paint brush icon" className="h-32 w-32 m-auto mt-5" />
           </div>
           <div className={projectTwoHover ? "flex flex-col m-auto items-center duration-1000 ease-in-out" : "flex flex-col m-auto items-center opacity-0 duration-700 ease-in-out transform scale-75"}>
             <h2 className="text-2xl font-semibold text-center">Palette Picker</h2>
             <p className="w-2/3 m-5 text-center text-base">Create and manage beautiful color palettes.</p>
-            <a href={projectData[1].link} target="_blank" className="w-2/3 p-2 text-center border rounded-full hover:bg-white hover:text-black">Go Live</a>
+            <a href={projectData[1].link} target="_blank" role="button" className="w-2/3 p-2 text-center border rounded-full hover:bg-white hover:text-black">Go Live</a>
           </div>
         </div>
 
@@ -74,20 +74,19 @@ export const Projects: React.FC = () => {
             <h2 className="mt-3 text-3xl font-semibold text-center">Iso-Challenge</h2>
             <p className="w-1/2 m-2 text-center text-base">A game of meaningless basketball knowledge.</p>
             <h3 className="w-1/2 m-2 text-center text-base">Click the ball to play!</h3>
-            <a href={projectData[3].link} target="_blank"><img src={projectIcons.basketball} alt="" className="h-20 w-20 m-auto hover:animate-spin" /></a>
+            <a href={projectData[3].link} target="_blank" role="button"><img src={projectIcons.basketball} alt="Basketball button" className="h-20 w-20 m-auto hover:animate-spin" /></a>
           </div>
         </div>
 
 
-        <div className="fitlitBackground flex flex-row h-64 w-full rounded shadow-2xl cursor-pointer"
+        <div className="fitlitBackground flex flex-col h-64 w-full rounded shadow-2xl cursor-pointer"
           onMouseEnter={toggleProjectFourHover}
           onMouseLeave={toggleProjectFourHover}
         >
-          <img src={projectIcons.lightning} alt="" className="h-8 w-8 ml-2 mt-3" />
-          <h2 className="text-white text-4xl font-bold">FitLit</h2>
-          <div className={projectFourHover ? "m-auto items-center duration-1000 ease-in-out" : "m-auto items-center opacity-0 duration-700 ease-in-out transform scale-75"}>
-            <p className="w-2/3 m-5 text-white text-base font-semibold">An application for both adults and kids to track their fitness metrics.</p>
-            <a href={projectData[2].link} target="_blank" className="w-2/3 p-2 text-white border rounded-full hover:bg-white hover:text-black">Go Live</a>
+          <h2 className="inline-block text-4xl font-bold"><img src={projectIcons.lightning} alt="" className="inline-block h-8 w-8" />FitLit</h2>
+          <div className={projectFourHover ? "flex flex-col justify-center m-4 duration-1000 ease-in-out" : "flex flex-col justify-center m-4 opacity-0 duration-700 ease-in-out transform scale-75"}>
+            <p className="w-1/3 m-3 text-base font-semibold">An application for both adults and kids to track their fitness metrics.</p>
+            <a href={projectData[2].link} target="_blank" role="button" className="w-1/3 p-2 text-center border rounded-full hover:bg-white hover:text-black">Go Live</a>
           </div>
         </div>
       </section>
